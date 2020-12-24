4b5d23e6b06142e4692403ed99db47a095bb0626
Idanturjeman <idan_turjeman29@walla.co.il>
2020-12-24 13:29:04 +0200

    add insertion functions

--- isort/insertion.c
+++ isort/insertion.c
@@ -9,3 +9,21 @@ void shift_element(int* arr, int i){
   }
   *arr = tmp;
 }
+
+void insertion_sort(int* arr , int len){
+  for(int sortIndex = 0; sortIndex < len; sortIndex++){
+    int minIndex = findMin(arr, len, sortIndex);
+    shift_element(arr+sortIndex, minIndex-sortIndex);
+  }
+}
+
+int findMin(int* arr , int len, int from){
+  int min = from;
+  from++;
+  while (from < len) {
+    if(*(arr+from) < *(arr+min))
+      min = from;
+    from++;
+  }
+  return min;
+}
