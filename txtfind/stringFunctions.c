cea3297d7ad90f51bc78c02b600b53a6f9750401
Idanturjeman <idan_turjeman29@walla.co.il>
2020-12-24 15:38:17 +0200

    add getline function

--- txtfind/stringFunctions.c
+++ txtfind/stringFunctions.c
@@ -1,3 +1,45 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+
+#ifndef LINE
+#define LINE 256
+#endif
+
+#ifndef PREV
+#define PREV -1
+#endif
+
+#ifndef NEXT
+#define NEXT +1
+#endif
+
+#ifndef WORD
+#define WORD 30
+#endif
+
+bool isEOF(bool b){
+  static bool eof = false;
+  if(b)
+    eof = true;
+  return eof;
+}
+
+int getLine(char s[]){
+  if(isEOF(false))
+    return EOF;
+
+  int i = 0;
+  char c;
+  do{
+    c = getchar();
+    *(s+i++) = c;
+  }while(c != '\n' && c != '\0' && i < LINE PREV);
+
+  *(s+i) = '\0';
+
+  if(isEOF(c == EOF))
+    return EOF;
+
+  return i PREV;
+}
